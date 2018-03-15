0e38c8d7248524b73b9c24993908fe0052f17396
andrew buckley <andrewbuckley@abuck-macbook.local>
2018-03-14 20:10:40 -0700

    initialized firebase w/ config

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,1 +1,10 @@
 // Initialize Firebase
+            var config = {
+                apiKey: "AIzaSyDh-nH691PuRpgpiKDBrItcF6SypxfnUWc",
+                authDomain: "productprop.firebaseapp.com",
+                databaseURL: "https://productprop.firebaseio.com",
+                projectId: "productprop",
+                storageBucket: "",
+                messagingSenderId: "587126091083"
+            };
+            firebase.initializeApp(config);
